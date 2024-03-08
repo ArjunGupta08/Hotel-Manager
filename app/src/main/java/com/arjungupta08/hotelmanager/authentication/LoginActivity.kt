7315df60d9f5377391d999e823c086b67535697c
Arjun Gupta <arjungupta0817@gmail.com>
2024-03-09 00:31:33 +0530

    LoginActivity

---
+++ app/src/main/java/com/arjungupta08/hotelmanager/authentication/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.arjungupta08.hotelmanager.authentication
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.arjungupta08.hotelmanager.R
+import com.arjungupta08.hotelmanager.databinding.ActivityLoginBinding
+import com.arjungupta08.hotelmanager.utils.leftInAnimation
+import com.arjungupta08.hotelmanager.utils.rightInAnimation
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var bindingMobile : ActivityLoginBinding
+
+    // Fonts
+    private lateinit var roboto : Typeface
+    private lateinit var robotoBold : Typeface
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingMobile = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bindingMobile.root)
+
+        bindingMobile.openLoginScreen.setOnClickListener {
+            replaceMobileFragment(LoginFragment())
+            leftInAnimation( bindingMobile.authFragContainerMobile, applicationContext)
+            setText(bindingMobile.openSignUpScreen,"New here?\nSign up",12f,roboto,R.color.black,R.drawable.rounded_corner_right_grey_background)
+
+            setText(bindingMobile.openLoginScreen,"Log in",14f,robotoBold,R.color.white,R.drawable.rounded_corner_left_black_background)
+            bindingMobile.openLoginScreen.elevation = 9f
+            bindingMobile.openSignUpScreen.elevation = 0f
+        }
+
+        bindingMobile.openSignUpScreen.setOnClickListener {
+            replaceMobileFragment(SignUpFragment())
+            rightInAnimation(bindingMobile.authFragContainerMobile, applicationContext)
+            setText(bindingMobile.openSignUpScreen,"Sign up",14f,robotoBold,R.color.white,R.drawable.rounded_corner_left_black_background)
+            bindingMobile.openSignUpScreen.elevation = 9f
+            bindingMobile.openLoginScreen.elevation = 0f
+
+            setText(bindingMobile.openLoginScreen,"Existing User?\nLog in",12f,roboto,R.color.black,R.drawable.rounded_corner_right_grey_background)
+
+        }
+    }
+
+    private fun replaceMobileFragment(fragment: Fragment) {
+        if (fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.authFragContainerMobile,fragment)
+            transaction.commit()
+        }
+    }
+
+    private fun setText(textView: TextView, text:String, size: Float, typFace: Typeface, color:Int, setBackground:Int ){
+        textView.text = text
+        textView.textSize = size
+        textView.typeface = typFace
+        textView.setTextColor(ContextCompat.getColor(applicationContext,color))
+        textView.background = ContextCompat.getDrawable(applicationContext,setBackground)
+    }
+
+}
---
+++ app/src/main/java/com/arjungupta08/hotelmanager/utils/AnimUtils.kt
@@ -0,0 +1,28 @@
+package com.arjungupta08.hotelmanager.utils
+
+import android.content.Context
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.arjungupta08.hotelmanager.R
+
+
+fun leftInAnimation(view: View, context: Context) {
+    // load the animation
+    val animSlideIn: Animation = AnimationUtils.loadAnimation(
+        context,
+        R.anim.l_to_r_in_animation
+    )
+    // start the animation
+    view.startAnimation(animSlideIn)
+}
+
+fun rightInAnimation(view: View, context: Context) {
+    // load the animation
+    val animSlideIn: Animation = AnimationUtils.loadAnimation(
+        context,
+        R.anim.right_in_animation
+    )
+    // start the animation
+    view.startAnimation(animSlideIn)
+}
