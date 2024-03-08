556c1c27011750c71827625716db464a4cdb4fc8
Arjun Gupta <arjungupta0817@gmail.com>
2024-03-08 23:36:19 +0530

    Splash Screen

--- app/src/main/java/com/arjungupta08/hotelmanager/MainActivity.kt
+++ app/src/main/java/com/arjungupta08/hotelmanager/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.arjungupta08.hotelmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.arjungupta08.hotelmanager.authentication.LoginActivity
+import com.arjungupta08.hotelmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+        }, 1000)
+
     }
 }
