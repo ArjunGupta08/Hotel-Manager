141bdb965bd1e43179a19e2dcb4038c4bf226661
Arjun Gupta <arjungupta0817@gmail.com>
2024-04-09 18:43:26 +0530

    Login using Firebase

--- app/src/main/java/com/arjungupta08/hotelmanager/onboarding/authentication/LoginFragment.kt
+++ app/src/main/java/com/arjungupta08/hotelmanager/onboarding/authentication/LoginFragment.kt
@@ -1,19 +1,28 @@
 package com.arjungupta08.hotelmanager.onboarding.authentication
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.arjungupta08.hotelmanager.DashboardActivity
 import com.arjungupta08.hotelmanager.databinding.FragmentLoginBinding
 import com.arjungupta08.hotelmanager.utils.shakeAnimation
-import java.lang.Exception
+import com.arjungupta08.hotelmanager.utils.showProgressDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
     private lateinit var bindingMobile : FragmentLoginBinding
 
     private lateinit var email : String
     private lateinit var password : String
+
+    private lateinit var progressDialog : Dialog
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,14 +48,28 @@ class LoginFragment : Fragment() {
             } else {
                 bindingMobile.emailLayout.isErrorEnabled = false
                 bindingMobile.passwordLayout.isErrorEnabled = false
+
                 email = bindingMobile.emailText.text.toString()
                 password = bindingMobile.passwordText.text.toString()
-                try {
-//                    login()
-                } catch (e: Exception) {
-                    println(e.toString())
-                }
+
+                progressDialog = showProgressDialog(requireContext())
+                login()
             }
         }
+    }
+
+    private fun login() {
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(context, DashboardActivity::class.java))
+                activity?.finish()
+            }
+            .addOnFailureListener {
+                progressDialog.dismiss()
+                Log.e("LogIN", it.message.toString())
+                Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+            }
     }
 }
